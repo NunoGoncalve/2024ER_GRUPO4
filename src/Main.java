07b1948d3a52e6d730361157e0e14c240d5db661
NunoGoncalve <1241621@isep.ipp.pt>
2025-01-06 21:26:59 +0000

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void menuLivros(){
-        int op;
-        Scanner ler = new Scanner(System.in);
-        Livros livros=new Livros();
-        livros.ler_livros();
-        do {
-            System.out.println("- Livros -");
-            System.out.println("1) - Listar");
-            System.out.println("2) - Adicionar");
-            System.out.println("3) - Pesquisar");
-            System.out.println("4) - Atualizar");
-            System.out.println("5) - Eliminar");
-            System.out.println("6) - Sair");
-            System.out.print("Selecione uma opção: ");
-            op = ler.nextInt();
-            switch (op) {
-                case 1:
-                    livros.listarLivros();
-                    break;
-                case 2:
-                    livros.adicionarLivro();
-                    break;
-
-                case 3:
-                    livros.listaLivro();
-                    break;
-                case 4:
-                    livros.atualizarLivro();
-                    break;
-
-                case 5:
-                    livros.eliminarLivro();
-                    break;
-
-                case 6:
-                    livros.guardarLivros();
-                    livros.limparLivros();
-                    System.out.println("A sair do menu livro...");
-                    break;
-                default:
-                    System.out.println("Opção incorreta! Tente novamente");
-                    break;
-            }
-        }while (op != 6);
-    }
-
     public static void menu(){
         Scanner ler = new Scanner(System.in);
         int op;
@@ -62,7 +16,7 @@ public class Main {
             op=ler.nextInt();
             switch(op){
                 case 1:
-                    menuLivros();
+                    /**/
                     break;
 
                 case 2:
